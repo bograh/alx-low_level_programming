bcd36a2b06c9768b6ea1e7a644e64092921f8191
Bernard Ograh <69661398+bograh@users.noreply.github.com>
2023-07-18 23:57:37 +0000

    Changes made to 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,23 @@
 #include"main.h"
 
 /**
- * _isalpha - checks if character is a letter
- *            both lowercase or uppercase
+ * _isalpha - checks if the character is a letter
+ *            both lowercase and uppercase
  *
  * @c: takes input from other functions.
  *
  * Return: 1 is c if true else 0
 */
 
-int _isalpha(int c)
-{
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
-		return (1);
-	return (0);
+// int _isalpha(int c)
+// {
+// 	if (c >= 97 && c <= 122 &&
+// 	    c >= 65 && c >= 90)
+// 		return (1);
+// 	return (0);
+// }
+
+int _isalpha(int c) {
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
 }
 
