72e9aac246c0f5cbf98e1d10fb4873b51234ee85
Bernard Ograh <69661398+bograh@users.noreply.github.com>
2023-07-18 23:58:54 +0000

    Changes to 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,13 +9,15 @@
  * Return: 1 is c if true else 0
 */
 
-// int _isalpha(int c)
-// {
-// 	if (c >= 97 && c <= 122 &&
-// 	    c >= 65 && c >= 90)
-// 		return (1);
-// 	return (0);
-// }
+/**
+* int _isalpha(int c)
+* {
+*	if (c >= 97 && c <= 122 &&
+*	    c >= 65 && c >= 90)
+*		return (1);
+*	return (0);
+* }
+*/
 
 int _isalpha(int c) {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
