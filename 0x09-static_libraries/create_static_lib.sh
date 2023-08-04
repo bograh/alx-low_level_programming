92c2eb1a630c5c26a9effea4fc39eb06224053af
Bernard Ograh <69661398+bograh@users.noreply.github.com>
2023-08-04 00:58:57 +0000

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-gcc -c *.c
-ar rc liball.a *.o
+
+# Compile each .c file into .o (object) files
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create the static library liball.a from the .o files
+ar -rc liball.a *.o
+
+# Index the library to improve linking times
 ranlib liball.a
+
+# Remove the .o files to clean up
+rm -f *.o
